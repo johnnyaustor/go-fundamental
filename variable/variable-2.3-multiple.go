1e207dd55d0c38541f1ad0b5518a292acc95de9d
Johnny Austor <johnny.austor@gmail.com>
2022-09-17 17:41:53 +0700

    2.3 Multiple Variable

---
+++ variable/variable-2.3-multiple.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	var namaDepan, namaBelakang string = "Johnny", "Austor"
+	var tanggal, bulan, tahun int = 13, 12, 2025
+	println(namaDepan, namaBelakang)
+	println(tanggal, bulan, tahun)
+}
